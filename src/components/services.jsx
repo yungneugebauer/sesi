d352dd6f21ab7412cd776d353eed695d527eed75
yungneugebauer <yungnex@gmail.com>
2020-08-04 09:11:24 -0300

    .

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -17,7 +17,7 @@ export class Services extends Component {
               ? this.props.data.map((d, i) => (
                   <div  key={`${d.name}-${i}`} className="col-md-4">
                     {" "}
-                    <i className={d.icon} onClick={()=>{alert('oi')}} > </i>
+                    <i className={d.icon} onClick={()=>{window.open('pagina.html','pagename')}} > </i>
                     <div className="service-desc">
                       <h3>{d.name}</h3>
                       <p>{d.text}</p>
